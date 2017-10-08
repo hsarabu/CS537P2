1226fd492044dd2c2aed7ed1aaeb91c264f9e8f9
Hitesh <hsarabu@wisc.edu>
2017-10-08 11:37:48 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/CS537P2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS537P2.dir/mysh.c.o"
+  "CS537P2.pdb"
+  "CS537P2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS537P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
